5b90f23494eda5fa70cfdcf4cce8c2e876bb21b2
Lohran Mendes <lohranfm@gmail.com>
2024-10-13 12:54:02 -0300

    VRS 1.0.0

--- script.js
+++
@@ -1,9 +0,0 @@
-import { initTabNav } from './modules/animacoes.js'
-import { initAccordion } from './modules/accordion.js'
-import { initScrollSuave } from './modules/scroll-suave.js'
-import { initAnimacaoScroll } from './modules/scroll-animacao.js'
-
-initTabNav();
-initAccordion();
-initScrollSuave();
-initAnimacaoScroll();
